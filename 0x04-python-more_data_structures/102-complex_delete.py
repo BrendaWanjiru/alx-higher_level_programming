0e5322ce754e1c3c4553619baae205d0fca75e36
BrendaWanjiru <brendawanjiru5@gmail.com>
2023-08-18 19:36:41 +0000

    python more data structures

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -3,4 +3,4 @@ def complex_delete(a_dictionary, value):
     for key, val in sorted(a_dictionary.items()):
         if val == value:
             del a_dictionary[key]
-    return a_dictionary
+    return none
